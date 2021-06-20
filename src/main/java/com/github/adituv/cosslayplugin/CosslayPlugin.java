8442bd203b039e0abeb443b8af30b587a0c89bea
AdituV <aditu.venyhandottir@gmail.com>
2021-06-20 22:56:17 +0100

    WIP fake chatheads

--- src/main/java/com/github/adituv/cosslayplugin/CosslayPlugin.java
+++ src/main/java/com/github/adituv/cosslayplugin/CosslayPlugin.java
@@ -1,14 +1,11 @@
 package com.github.adituv.cosslayplugin;
 
+import com.github.adituv.cosslayplugin.model.KitColor;
 import javax.inject.Inject;
 import javax.inject.Provider;
-
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
-import net.runelite.api.events.InteractingChanged;
-import net.runelite.api.events.WidgetClosed;
-import net.runelite.api.events.WidgetLoaded;
-import net.runelite.api.widgets.*;
+import net.runelite.api.events.CommandExecuted;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.chatbox.ChatboxPanelManager;
@@ -31,82 +28,32 @@ public class CosslayPlugin extends Plugin
 	private ChatboxPanelManager chatboxPanelManager;
 
 	@Inject
-	private Provider<FakeNpcDialogInput> fakeNpcDialogInputProvider;
-
-	private int npcToOverride = NpcID.GRAND_EXCHANGE_CLERK;
-	private Widget originalDialog = null;
-	private boolean overrideDialogShowing = false;
-
-	// state to detect a new dialog with an npc
-	private NPC playerInteractingWith = null;
-	private boolean waitingForDialog = false;
+	private Provider<FakeDialogInput> fakeDialogInputProvider;
 
 	@Subscribe
-	private void onInteractingChanged(InteractingChanged e)
+	private void onCommandExecuted(CommandExecuted e)
 	{
-		if (e.getSource().equals(client.getLocalPlayer()))
-		{
-			Actor target = e.getTarget();
-			if (target instanceof NPC)
-			{
-				playerInteractingWith = (NPC) target;
-				log.debug("Player interacting with: {}", playerInteractingWith.getName());
 
-				waitingForDialog = true;
-			}
-			else
-			{
-				playerInteractingWith = null;
-			}
+		int[] equipOverrides = new int[] {ItemID.MASK_OF_BALANCE + 512, -1, -1, -1, -1, -1, -1, -1, 256, -1, -1, -1};
+		int[] colorOverrides = new int[] {-1, -1, -1, -1, KitColor.COLOR_BLACK.getSkinId()};
+
+		if (e.getCommand().equalsIgnoreCase("cosmo"))
+		{
+			FakeDialogInput dialog1 = fakeDialogInputProvider.get()
+				.type(DialogType.DIALOG_HEAD_LEFT)
+				.player()
+				.speakerName("Cosmo")
+				.overrides(equipOverrides, colorOverrides)
+				.message("You are not ready.  I will contact you when it is time.");
+
+			FakeDialogInput dialog2 = fakeDialogInputProvider.get()
+				.type(DialogType.DIALOG_HEAD_RIGHT)
+				.player()
+				.message("Well, that was strange.");
+
+			dialog1.next(dialog2);
+
+			dialog1.build();
 		}
 	}
-
-	@Subscribe
-	private void onWidgetLoaded(WidgetLoaded e)
-	{
-
-		if (waitingForDialog && isDialogWidget(e.getGroupId()))
-		{
-			log.debug("Dialog started with {}", playerInteractingWith.getName());
-			waitingForDialog = false;
-
-			if (playerInteractingWith.getId() == npcToOverride)
-			{
-
-				originalDialog = client.getWidget(e.getGroupId(), 0);
-				originalDialog.setHidden(true);
-
-				fakeNpcDialogInputProvider
-					.get()
-					.npc(npcToOverride)
-					.message("This is a fake dialog injected right before the real<br>npc dialog starts.")
-					.onClose(() ->
-						originalDialog.setHidden(false)
-					).build();
-			}
-		}
-	}
-
-	@Subscribe
-	private void onWidgetClosed(WidgetClosed e)
-	{
-		if (overrideDialogShowing && isDialogWidget(e.getGroupId()))
-		{
-			chatboxPanelManager.getContainerWidget().deleteAllChildren();
-		}
-	}
-
-	private boolean isDialogWidget(int groupId)
-	{
-		final int DIALOG_NPC_GROUP_ID = WidgetInfo.DIALOG_NPC.getGroupId();
-		final int DIALOG_PLAYER_GROUP_ID = WidgetInfo.DIALOG_PLAYER.getGroupId();
-		final int DIALOG_OPTION_GROUP_ID = WidgetInfo.DIALOG_OPTION.getGroupId();
-
-		return (
-			groupId == DIALOG_NPC_GROUP_ID
-				|| groupId == DIALOG_PLAYER_GROUP_ID
-				|| groupId == DIALOG_OPTION_GROUP_ID
-		);
-	}
-
 }
